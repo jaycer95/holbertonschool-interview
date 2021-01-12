02ec92e35231d8a37146f711264987eb5d98183c
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-12 02:25:00 -0800

    Check

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,92 +1,116 @@
 #include "sandpiles.h"
-
+/**
+* verification - verify if sandpile is stable
+* @a: sandpile
+* Return: true or false
+**/
 bool verification(int a[3][3])
 {
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (a[i][j] > 3)
-            {
-                return (false);
-            }
-        }
-    }
-    return (true);
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+if (a[i][j] > 3)
+{
+return (false);
 }
+}
+}
+return (true);
+}
+/**
+* print - print grid
+* @a: sandpile
+* Return: void
+**/
 void print(int a[3][3])
 {
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%d", a[i][j]);
-            if (j < 2)
-            {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
-    printf("\n=\n\n");
+printf("=\n");
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+printf("%d", a[i][j]);
+if (j < 2)
+{
+printf(" ");
 }
-
+}
+printf("\n");
+}
+}
+/**
+* sum - sum of two sandpiles
+* @a: sandpile
+* @b: sandpile
+* Return: void
+**/
 void sum(int a[3][3], int b[3][3])
 {
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            a[i][j] += b[i][j];
-        }
-    }
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+a[i][j] += b[i][j];
 }
+}
+}
+/**
+* stable - stabalize sandpile
+* @a: sandpile
+* @tmp: copy of sandpile a
+* Return: void
+**/
 void stable(int a[3][3], int tmp[3][3])
 {
-    for(int i=0; i<3; i++)
-    {
-        for(int j=0; j<3; j++)
-        {
-        tmp[i][j] = a[i][j];
-        }
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-
-            if (tmp[i][j] >= 4)
-            {
-                a[i][j] = a[i][j] - 4;
-                if (i >= 1)
-                {
-                    a[i - 1][j] += 1;
-                }
-                if (i <= 1)
-                {
-                    a[i + 1][j] += 1;
-                }
-                if (j >= 1)
-                {
-                    a[i][j - 1] += 1;
-                }
-                if (j <= 1)
-                {
-                    a[i][j + 1] += 1;
-                }
-            }
-        }
-    }
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+tmp[i][j] = a[i][j];
 }
-
+}
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+if (tmp[i][j] >= 4)
+{
+a[i][j] = a[i][j] - 4;
+if (i >= 1)
+{
+a[i - 1][j] += 1;
+}
+if (i <= 1)
+{
+a[i + 1][j] += 1;
+}
+if (j >= 1)
+{
+a[i][j - 1] += 1;
+}
+if (j <= 1)
+{
+a[i][j + 1] += 1;
+}
+}
+}
+}
+}
+/**
+* sandpiles_sum - stabalized sum of two sandpiles
+* @grid1: sandpile 1
+* @grid2: sandpile 2
+* Return: void
+**/
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int tmp[3][3];
+int tmp[3][3];
 
-    sum(grid1, grid2);
-    while (!verification(grid1))
-    {
-        print(grid1);
-        stable(grid1, tmp);
-    }
+sum(grid1, grid2);
+while (!verification(grid1))
+{
+print(grid1);
+stable(grid1, tmp);
+}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -6,7 +6,7 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 bool verification(int a[3][3]);
-void sum(int a[3][3],int b[3][3]);
-void stable (int a[3][3], int tmp[3][3]);
+void sum(int a[3][3], int b[3][3]);
+void stable(int a[3][3], int tmp[3][3]);
 
 #endif
