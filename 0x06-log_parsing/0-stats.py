4c830087b40b4b6d87442df72671e2f512164563
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-31 14:20:50 -0800

    check

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" log parsing """
+
+
+import sys
+
+
+status = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0}
+p = 1
+fsize = 0
+try:
+    for line in sys.stdin:
+        while p < 10:
+            p += 1
+        split = line.split()
+        try:
+            fsize += int(split[-1])
+        except Exception as e:
+            pass
+        try:
+            for key, value in status.items():
+                if key == split[-2]:
+                    status[key] = status[key] + 1
+        except Exception as e:
+            pass
+        print("File size: {}".format(fsize))
+        for key, value in sorted(status.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
+        p = 1
+
+except KeyboardInterrupt as e:
+    print("File size: {}".format(fsize))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
