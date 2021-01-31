f3df9ad4cda45fcbf7a3ad04975a74b7179f7c0d
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-31 14:31:32 -0800

    check

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -8,27 +8,20 @@ import sys
 status = {
     "200": 0, "301": 0, "400": 0, "401": 0,
     "403": 0, "404": 0, "405": 0, "500": 0}
-p = 1
+p = 0
 fsize = 0
 try:
     for line in sys.stdin:
-        if p == 10:
-            print("File size: {}".format(fsize))
-            for key, value in sorted(status.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
-            p = 1
-        else:
-            p += 1
         split = line.split()
         fsize += int(split[-1])
         for key, value in status.items():
                 if split[-2] == key:
                     status[key] = status[key] + 1
-    print("File size: {}".format(fsize))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+        if p % 10 == 0:
+            print("File size: {}".format(fsize))
+            for key, value in sorted(status.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
 
 except KeyboardInterrupt:
     pass
