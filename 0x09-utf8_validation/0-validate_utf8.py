7a310bc9eafe36e2006151b967e6107773ae61f8
jaycer95 <jacer.dabbabi95@gmail.com>
2021-02-18 11:51:54 -0800

    0

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Utf8 Validator """
+
+
+
+def validUTF8(data):
+    """ check if valid utf8 format """
+    try:
+        bytes(data).decode("UTF-8")
+        return True
+    except Exception:
+        return False
