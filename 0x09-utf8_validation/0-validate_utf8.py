dd43cc4e4a866d16cc00c8675aeb44138d4c8583
jaycer95 <jacer.dabbabi95@gmail.com>
2021-02-18 12:20:14 -0800

    0

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,9 @@
 
 def validUTF8(data):
     """ check if valid utf8 format """
+    m = data
     try:
-        for n in data:
-            n &= 255
-        bytes(data).decode("UTF-8")
+        bytes(m).decode("UTF-8")
         return True
     except Exception:
         return False
