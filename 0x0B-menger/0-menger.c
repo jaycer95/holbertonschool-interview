40c751759bd683a54d6278869b51b2b5d0b2caa0
jaycer95 <jacer.dabbabi95@gmail.com>
2021-03-04 12:23:58 -0800

    Push

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,27 @@
+#include "menger.h"
+/**
+* menger - print menger
+* @level: level
+* Return: void
+**/
+void menger(int level){
+int size = pow(3, level);
+int i, j, k, m;
+char diez;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+diez = '#';
+k = i;
+for(m = j; m; m /= 3)
+{
+if (k % 3 == 1 && m % 3 == 1)
+diez = ' ';
+k = k / 3;
+}
+printf("%c", diez);
+}
+printf("\n");
+}
+}
