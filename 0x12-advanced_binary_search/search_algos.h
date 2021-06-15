d4a58e0dd40ed05e848bdb35b9091ed4b87ddc6c
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-16 00:24:26 +0100

    ADvanced Binary Search

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _ADVANCED_SEARCH_
+#define _ADVANCED_SEARCH_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, size_t i, size_t size);
+
+#endif
