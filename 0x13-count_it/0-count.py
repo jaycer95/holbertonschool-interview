1a9189ce0c91ba32e6746b2cea2e625ab6daed01
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-16 01:02:07 +0100

    0

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,32 +7,29 @@ def count_words(subreddit, word_list, count_dict={}, after=""):
     """ Query the Reddit API"""
     if len(count_dict) <= 0:
         for item in word_list:
-            count_dict[item.lower()] = 0
+            count_dict[item] = 0
 
     if after is None:
-        sorted_dict = dict(sorted(
-            count_dict.items(),
-            key=lambda x: (-x[1], x[0])
-            ))
-        for k, v in sorted_dict.items():
-            if v > 0:
-                print("{}: {}".format(k, v))
+        word_list = [[key, value] for key, value in count_dict.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for k  in word_list:
+            if k[1]:
+                print("{}: {}".format(k[0].lower(), k[1]))
         return None
     url = "https://api.reddit.com/r/{}/hot".format(subreddit)
     params = {'limit': 100, 'after': after}
-    headers = {'user-agent': 'xxxx'}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
+    headers = {'user-agent': 'my-app/0.0.1'}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code == 200:
         after = response.json().get("data").get("after")
         posts = response.json().get("data").get("children")
         for child in posts:
             title = child.get("data").get(
-                "title").lower().split(" ")
+                "title")
+            lower = [s.lower() for s in title.split(' ')]
             for word in word_list:
-                count_dict[word.lower()] += title.count(
-                    word.lower())
+                count_dict[word] += lower.count(word.lower())
     else:
         return None
     count_words(subreddit, word_list, count_dict, after)
