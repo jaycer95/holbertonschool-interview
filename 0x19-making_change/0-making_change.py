deb9c172de996482ed1787785bf03ac2974d5e95
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-23 00:46:23 +0100

    Check code

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Making Change """
+import numpy as np
+
+
+def makeChange(coins, total):
+    """ Determine the fewest number of coins needed to meet a given amount """
+    if total <= 0:
+        return 0
+    num = 0
+    r = total
+    sorted_coins = np.sort(np.array(coins))
+    while sorted_coins != []:
+        if r < sorted_coins.all():
+            return -1
+        num += r // sorted_coins[-1]
+        r = r % sorted_coins[-1]
+        if r == 0:
+            return num
+        sorted_coins = sorted_coins[:-1]
+    return -1
