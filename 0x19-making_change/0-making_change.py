8c242cb3b37f47a571b622e7c9b3584ca4be481f
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-23 19:58:22 +0100

    0

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -11,9 +11,14 @@ def makeChange(coins, total):
     r = total
     sorted_coins = np.sort(np.array(coins))
     while sorted_coins != [] and r > 0:
-        num += r // sorted_coins[-1]
-        r = r % sorted_coins[-1]
-        if r == 0:
-            return num
-        sorted_coins = sorted_coins[:-1]
-    return -1
+        try:
+            if r / sorted_coins[-1] >= 1:
+                num += r // sorted_coins[-1]
+                r = r % sorted_coins[-1]
+            else:
+                sorted_coins = sorted_coins[:-1]
+        except Exception:
+            return -1
+    if r != 0:
+        return -1
+    return num
