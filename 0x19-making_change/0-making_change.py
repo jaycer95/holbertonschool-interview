040cb10d00563668d446d65e2fbb6e5b7ee397f5
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-23 20:02:14 +0100

    0

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -9,15 +9,10 @@ def makeChange(coins, total):
     num = 0
     r = total
     coins.sort()
-    while coins != [] and r > 0:
-        try:
-            if r / coins[-1] >= 1:
-                num += r // coins[-1]
-                r = r % coins[-1]
-            else:
-                coins.pop()
-        except Exception:
-            return -1
-    if r != 0:
-        return -1
-    return num
+    while coins != []:
+        num += r // coins[-1]
+        r = r % coins[-1]
+        if r == 0:
+            return num
+        coins.pop()
+    return -1
